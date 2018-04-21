cc1ce2e4a119af3768176d91eea70fdfd5c9e6df
Mohammadreza Barazesh <barazesh.mr90@gmail.com>
2018-04-21 15:44:46 +0430

    add Grid class

---
+++ WindowsFormsApp1/WindowsFormsApp1/Grid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Grid
+    {
+        public State[] States = new State[2];
+        private int Dim1;
+        private int Dim2;
+
+        public Grid(int d1, int d2)
+        {
+            Dim1 = d1;
+            Dim2 = d2;
+        }
+    }
+}
