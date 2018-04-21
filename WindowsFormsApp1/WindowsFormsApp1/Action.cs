aa7b80b5b457cad374490efb9b7b6e88520321b1
Mohammadreza Barazesh <barazesh.mr90@gmail.com>
2018-04-21 15:16:40 +0430

    adding the first classes

---
+++ WindowsFormsApp1/WindowsFormsApp1/Action.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Action
+    {
+        public int NextState { get; set; }
+        public double Reward { get; set; }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/State.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class State
+    {
+        private double _Value;
+
+        public double Value
+        {
+            get { return _Value; }
+            set { _Value = value; }
+        }
+
+        Action Up = new Action();
+        Action Down = new Action();
+        Action Right = new Action();
+        Action Left = new Action();
+
+
+
+    }
+}
